79f4569d8118e654b2d37450070cad317b87b70b
Athas @ara <83549525+it20301422@users.noreply.github.com>
2024-02-09 21:36:23 +0530

    first commit

---
+++ Service/StudentService.cs
@@ -0,0 +1,48 @@
+﻿using Data_table.Context;
+using Data_table.IService;
+using Data_table.Models;
+
+namespace Data_table.Service
+{
+    public class StudentService : IStudentService
+    {
+        private readonly DatabaseContext _context;
+        public StudentService(DatabaseContext context) 
+        { 
+            _context = context;
+        
+        }
+        public string Delete(int studentId)
+        {
+            var student = _context.Students.FirstOrDefault(x => x.StudentId == studentId);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+            }
+            return "Deleted";
+        }
+
+        public Student GetById(int studentId) 
+        {
+            return _context.Students.FirstOrDefault(x => x.StudentId == studentId);
+        }
+
+        public List<Student> GetStudents()
+        {
+            return _context.Students.ToList();
+                
+        }
+
+        public void Save(Student oStudent)
+        {
+            _context.Students.Add(oStudent);
+            _context.SaveChanges();
+        }
+
+        public void Update(Student oStudent)
+        {
+            _context.Students.Add(oStudent);
+            _context.SaveChanges();
+        }
+    }
+}
